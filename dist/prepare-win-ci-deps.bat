241d6c9699ad9641e5eff51db29d017568fe5dad
CrLF0710 <crlf0710@gmail.com>
2019-11-16 18:39:28 +0800

    Fix missing directory.

--- dist/prepare-win-ci-deps.bat
+++ dist/prepare-win-ci-deps.bat
@@ -10,3 +10,4 @@ git remote add origin https://github.com/crlf0710/tectonic-prebuilt-deps.git
 git fetch origin master
 git checkout -f -B master origin/master
 FOR /D %%G in ("packages\*") DO xcopy /e /f /y "%%~G\*" "installed\%VCPKG_DEFAULT_TRIPLET%\"
+if not exist installed\vcpkg\updates mkdir installed\vcpkg\updates
