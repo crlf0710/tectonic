d1858b65f1013077ec9346c2b8b25525cbcbe785
Mrmaxmeier <Mrmaxmeier@gmail.com>
2019-11-18 15:38:05 +0100

    re-introduce `xetex_snprintf` to fix windows linkage

--- engine/tectonic/stub_stdio.c
+++ engine/tectonic/stub_stdio.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 
 int xetex_sprintf ( char * str, const char * format, ... );
+int xetex_snprintf ( char * str, size_t n, const char * format, ... );
 int xetex_strcasecmp(const char *s1, const char *s2);
 
 int xetex_sprintf ( char * str, const char * format, ... ) {
@@ -10,6 +11,15 @@ int xetex_sprintf ( char * str, const char * format, ... ) {
     int r;
     va_start(arglist, format);
     r = vsprintf(str, format, arglist);
+    va_end(arglist);
+    return r;
+}
+
+int xetex_snprintf ( char * str, size_t n, const char * format, ... ) {
+    va_list arglist;
+    int r;
+    va_start(arglist, format);
+    r = vsnprintf(str, n, format, arglist);
     va_end(arglist);
     return r;
 }
